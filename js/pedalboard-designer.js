781aa8da31c389a5dd47effda0439a623f2793e9
Kevin Stine <ikaikastine@gmail.com>
2016-06-27 10:16:24 -0700

    Added a comment
    to specify what the function does for future reference :raised_hands:

--- js/pedalboard-designer.js
+++ js/pedalboard-designer.js
@@ -1,3 +1,4 @@
+// Function to change the text of the dropdown once a selection has been made
 $(".dropdown-menu li a").click(function() {
   $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
   $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
